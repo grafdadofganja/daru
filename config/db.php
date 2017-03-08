32e5447d4bc06024e964cf67eaddc39349171a8d
grafdadofganja <dadofganja@mail.ru>
2017-03-08 16:56:30 +0500

    new site
    allahu akbar

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlite:@app/sqlite.db'
+    // 'dsn' => 'mysql:host=localhost;dbname=test',
+    // 'username' => 'root',
+    // 'password' => '',
+    // 'charset' => 'utf8',
+];
