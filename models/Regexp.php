9e19888c2efe8291da4a2c3b7278486b0b90461e
zlapot <zloi.apot@yandex.ru>
2017-03-11 02:07:32 +0300

    add \s

--- models/Regexp.php
+++ models/Regexp.php
@@ -5,7 +5,7 @@ namespace app\models;
 class Regexp extends \yii\base\Object 
 {
     public static function generateLink($string) {
-        $pattern = '/\[([А-Яа-я0-9a-zA-Z]+)\|([a-z]+)\]/su';
+        $pattern = '/\[([А-Яа-я0-9a-zA-Z\s]+)\|([a-z]+)\]/su';
         $replace = '<a href="/daru/search?tag='.'$2'.'" class="gen-link">'.'$1'.'</a>';  
         return preg_replace($pattern, $replace, $string);
     }
