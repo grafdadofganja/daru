5cbb5df2405a0a2f30bbcd999661b8d17e19d6ab
zlapot <zloi.apot@yandex.ru>
2017-03-11 02:28:06 +0300

    edit link

--- models/Regexp.php
+++ models/Regexp.php
@@ -2,11 +2,15 @@
 
 namespace app\models;
 
+use yii\helpers\Html;
+
 class Regexp extends \yii\base\Object 
 {
     public static function generateLink($string) {
         $pattern = '/\[([А-Яа-я0-9a-zA-Z\s]+)\|([a-z]+)\]/su';
-        $replace = '<a href="/daru/search?tag='.'$2'.'" class="gen-link">'.'$1'.'</a>';  
+        //$replace = Html::a('$1', ['search', 'tag' => '$2'], ['class' => 'gen-link']);
+
+        $replace = '<a href="/daru/web/index.php/search?tag='.'$2'.'" class="gen-link">'.'$1'.'</a>';  
         return preg_replace($pattern, $replace, $string);
     }
 }
