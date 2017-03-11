b66134998a7e40d32deab096637183963b5e4ff7
zlapot <zloi.apot@yandex.ru>
2017-03-11 14:01:44 +0300

    regexp maintenance '-', 'ё' and 'Ё'

--- models/Regexp.php
+++ models/Regexp.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 class Regexp extends \yii\base\Object 
 {
     public static function generateLink($string) {
-        $pattern = '/\[([А-Яа-я0-9a-zA-Z\s]+)\|([a-z]+)\]/su';
+        $pattern = '/\[([А-Яа-я0-9a-zA-ZёЁ\-\s]+)\|([a-z]+)\]/su';
         //$replace = Html::a('$1', ['search', 'tag' => '$2'], ['class' => 'gen-link']);
 
         $replace = '<a href="/daru/web/index.php/search?tag='.'$2'.'" class="gen-link">'.'$1'.'</a>';  
