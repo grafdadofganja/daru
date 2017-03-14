693dded6ace3610717396ba8d658362e04ee5a25
zlapot <zloi.apot@yandex.ru>
2017-03-14 22:16:22 +0300

    add .htaccess

--- config/web.php
+++ config/web.php
@@ -10,6 +10,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'hvjhkj556Qh6',
+            //'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -46,12 +47,16 @@ $config = [
             'rules' => [
                 [
                     'pattern' => 'article',
-                    'route' => 'article/index',
-                    'suffix' => '.json',
+                    'route' => 'article/<action>',
+                    'suffix' => '.asp',
                 ],
             ],
         ],
-        
+        'assetManager' => [
+            'basePath' => '@webroot/assets',
+            'baseUrl' => '@web/assets'
+        ],
+                
     ],
     'params' => $params,
 ];
